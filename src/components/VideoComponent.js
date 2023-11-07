cc55e03d01b45649424a6b6845e34840ed8f49f2
Debkumar04 <shitdebkumar2003@gmail.com>
2023-11-07 14:22:15 +0530

    new update

---
+++ src/components/VideoComponent.js
@@ -0,0 +1,17 @@
+const VideoComponent=()=>{
+    return(
+    <div className="item">
+    <div className="thumbnail">
+        
+        {/* <img src={"https://i.ytimg.com/vi/Gbxiqswz1hA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAabX0wgjxiXzuFIoUZ0Z2oA8v2OA"} */}
+            {/* className="thumbnailImage" /> */}
+    </div>
+    <div className="title">
+        <div className="chlogo"></div>
+        <div className="tile">Hi I'm DEB KUMAR</div>
+    </div>
+    <div className="channel">DEB's channel</div>
+    <div className="views">98M views</div>
+</div>);
+}
+export default VideoComponent;
