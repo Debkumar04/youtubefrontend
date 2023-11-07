ec80cf0642f9827f9244c403716a7c69eec8eb24
Debkumar04 <shitdebkumar2003@gmail.com>
2023-11-07 22:11:00 +0530

    new update

---
+++ src/components/hookComponent.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+
+
+const HookComponent=()=>{
+
+    const [click , setClick]= useState(0);
+
+    const on
+    return(
+
+        <div>
+            <div>0</div>
+            <button>click</button>
+        </div>
+    )
+}
+export default HookComponent;
